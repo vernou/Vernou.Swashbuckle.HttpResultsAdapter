ebefeec521e10c2994cbfb395ba4dc836c937608
Cedric VERNOU <cedric.vernou@hotmail.com>
2023-10-28 02:33:23 +0200

    Compare responses with minimal API

---
+++ src/Vernou.Swashbuckle.HttpResultsAdapter.Demo.MinimalApi/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Vernou.Swashbuckle.HttpResultsAdapter.Demo.MinimalApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // As HttpResultsController
+        app.MapGetReturn<Ok>("ok");
+        app.MapGetReturn<Created>("created");
+        app.MapGetReturn<Accepted>("accepted");
+        app.MapGetReturn<AcceptedAtRoute>("acceptedatroute");
+        app.MapGetReturn<NoContent>("nocontent");
+        app.MapGetReturn<BadRequest>("badrequest");
+        app.MapGetReturn<NotFound>("notfound");
+        app.MapGetReturn<Conflict>("conflict");
+        //app.MapGetReturn<UnauthorizedHttpResult>("unauthorized");
+        app.MapGetReturn<UnprocessableEntity>("unprocessableentity");
+        //app.MapGetReturn<ValidationProblem>("validationproblem");
+
+        // As HttpResultsOfController
+        app.MapGetReturn<Ok<Foo>>("ok-foo");
+        app.MapGetReturn<Created<Foo>>("created-foo");
+        app.MapGetReturn<CreatedAtRoute<Foo>>("createdatroute-foo");
+        app.MapGetReturn<Accepted<Foo>>("accepted-foo");
+        app.MapGetReturn<BadRequest<Foo>>("badrequest-foo");
+        app.MapGetReturn<Conflict<Foo>>("conflict-foo");
+        app.MapGetReturn<NotFound<Foo>>("notfound-foo");
+        app.MapGetReturn<UnprocessableEntity<Foo>>("unprocessableentity-foo");
+
+        // As ManyHttpResultsController
+        app.MapGetReturn<Results<Ok, NoContent>>("ok_nocontent");
+        app.MapGetReturn<Results<Ok, NoContent, NotFound>>("ok_nocontent_notfound");
+
+        app.Run();
+    }
+}
+
+internal static class IEndpointRouteBuilderExtentions
+{
+    public static void MapGetReturn<TResult>(this IEndpointRouteBuilder endpoints, string path)
+    {
+        endpoints.MapGet(path, Return<TResult>);
+        endpoints.MapGet(path + "-async", Return<Task<TResult>>);
+    }
+
+    private static TResult Return<TResult>()
+        => throw new NotImplementedException();
+}
+
+public sealed class Foo
+{
+    public int Id { get; set; }
+    public string Label { get; set; } = "";
+}

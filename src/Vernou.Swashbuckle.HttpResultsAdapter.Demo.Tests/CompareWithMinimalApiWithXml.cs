22efbcd9259afc04e31ec39bcba24aa301117f55
Cedric VERNOU <cedric.vernou@hotmail.com>
2023-10-29 14:32:34 +0100

    Add test to check response from minimal API isn't modified

--- src/Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests/CompareWithMinimalApiWithXml.cs
+++ src/Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests/CompareWithMinimalApiWithXml.cs
@@ -1,27 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-
-namespace Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests;
+﻿namespace Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests;
 
 public class CompareWithMinimalApiWithXml : CompareWithMinimalApiBase
 {
-
-    public CompareWithMinimalApiWithXml() : base(ControllerOpenApiDocument, new[] { "application/json", "application/xml" })
+    public CompareWithMinimalApiWithXml() : base(OpenApiDocumentLocator.FromControllerApiWithXml, new[] { "application/json", "application/xml" })
     { }
-
-    private readonly static Task<OpenApiDocument> ControllerOpenApiDocument = OpenApiDocumentGetter.Get<ApiWithXmlFactory, Program>();
-
-    private class ApiWithXmlFactory : WebApplicationFactory<Program>
-    {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            base.ConfigureWebHost(builder);
-            builder.ConfigureTestServices(services =>
-                services.AddControllers().AddXmlSerializerFormatters()
-            );
-        }
-    }
 }
---
+++ src/Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests/IgnoreMinimalApiEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Vernou.Swashbuckle.HttpResultsAdapter.Demo.Tests;
+
+public class IgnoreMinimalApiEndpoint
+{
+    [Theory]
+    [InlineData("/minimalapi/ok_204")]
+    [InlineData("/minimalapi/ok_204-async")]
+    public async Task CheckMinimalApiEndpointIsNotModified(string path)
+    {
+        // Arrange
+
+        var document = await OpenApiDocumentLocator.FromControllerApi;
+        var responses = document.Paths[path].Operations[OperationType.Get].Responses;
+
+        // Assert
+
+        var response = responses.ShouldHaveSingleItem();
+        response.Key.ShouldBe("204");
+        response.Value.Description.ShouldBe("Custom Description");
+        response.Value.Content.ShouldBeEmpty();
+    }
+}
